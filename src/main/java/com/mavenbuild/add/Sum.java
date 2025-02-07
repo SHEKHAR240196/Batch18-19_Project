b07379a676709ed93a88e877f5232563aa2e764d
SHEKHAR240196 <shekharpatle0@gmail.com>
2025-02-07 19:31:28 +0530

    this is added biuld tool project by shekhar

---
+++ src/main/java/com/mavenbuild/add/Sum.java
@@ -0,0 +1,9 @@
+package com.mavenbuild.add;
+
+public class Sum {
+	
+	public int sum(int a, int b) {
+		return a+b;
+	}
+
+}
---
+++ src/test/java/com/mavenbuild/BuildtoolApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mavenbuild;
+
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+	}
+
+}
